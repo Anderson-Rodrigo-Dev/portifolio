96be119575c29e90c0cfafcf38b0af8d35b008db
Anderson-Rodrigo-Dev <andersonrodrigodev@gmail.com>
2022-12-03 19:32:48 -0300

    feat: adicionando gh-pages

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <ThemeProvider>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <Sidebar />
           <ThemeTogglerButton />
           <Routes>
