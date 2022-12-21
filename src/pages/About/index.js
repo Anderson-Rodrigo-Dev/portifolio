78d705f448c76529f01a78784566fca93b80fbc6
Anderson-Rodrigo-Dev <andersonrodrigodev@gmail.com>
2022-12-21 17:38:57 -0300

    Adicionado curriculo

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -4,7 +4,6 @@ import { useThemeContext } from "../../hooks/useThemeToggler";
 import * as S from "./Styles";
 import { useMenuContext } from "../../hooks/useMenuContext";
 
-
 function getSessionStorageOrDefault(key, defaultValue) {
   const stored = sessionStorage.getItem(key);
   if (!stored) {
@@ -16,17 +15,20 @@ function getSessionStorageOrDefault(key, defaultValue) {
 const About = () => {
   const [clicked, setClicked] = useState(
     getSessionStorageOrDefault("download", false)
-    );
-    
-    useEffect(() => {
-      sessionStorage.setItem("download", JSON.stringify(clicked));
-    }, [clicked]);
-    
-    const { theme } = useThemeContext();
-    const showMenu = useMenuContext()
-    
+  );
+
+  useEffect(() => {
+    sessionStorage.setItem("download", JSON.stringify(clicked));
+  }, [clicked]);
+
+  const { theme } = useThemeContext();
+  const showMenu = useMenuContext();
+
   return (
-    <S.ContainerAbout style={{ backgroundColor: theme.background }} showmenu={showMenu}>
+    <S.ContainerAbout
+      style={{ backgroundColor: theme.background }}
+      showmenu={showMenu}
+    >
       <S.ContainerInfo>
         <S.AboutMe style={{ color: theme.color }}>Olá, eu sou</S.AboutMe>
         <S.TitleName>Anderson Rodrigo!</S.TitleName>
@@ -49,10 +51,7 @@ const About = () => {
             <S.IconDownload />
           </Button>
         ) : (
-          <a
-            href="https://export-download.canva.com/VvAyE/DAE6N3VvAyE/403/0-42817007312.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJHKNGJLC2J7OGJ6Q%2F20221201%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20221201T052810Z&X-Amz-Expires=59722&X-Amz-Signature=eb703482fde4f0883ff7ded716c85e30b6219e7101f1af4e4e7e1a603ef79d42&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%2A%3DUTF-8%27%27Desenvolvedor%2520front%2520end%2520-%2520Anderson%2520Rodrigo%2520de%2520Oliveira%2520Batista.pdf&response-expires=Thu%2C%2001%20Dec%202022%2022%3A03%3A32%20GMT"
-            download
-          >
+          <a href="../../../public/curriculo.pdf" download>
             <Button
               onClick={() => setClicked(true)}
               clicked={clicked}
