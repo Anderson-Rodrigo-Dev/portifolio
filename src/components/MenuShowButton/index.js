abf83ccd908e3a022b2ff57010004b7b47243bec
Anderson-Rodrigo-Dev <andersonrodrigodev@gmail.com>
2022-12-29 19:02:02 -0300

    Alterado o curriculo e organizado envio

--- src/components/MenuShowButton/index.js
+++ src/components/MenuShowButton/index.js
@@ -4,14 +4,15 @@ import MenuHamburguer from "../MenuHamburguer";
 export const MenuShowButton = () => {
   const { showMenu, setShowMenu } = useMenuContext();
 
-  const HandleMenu = (e) => {
+  const handleMenu = (e) => {
     e.preventDefault();
 
     setShowMenu(true);
   };
+
   return (
     <div>
-      <MenuHamburguer onClick={HandleMenu} showMenu={showMenu} />
+      <MenuHamburguer onClick={handleMenu} showMenu={showMenu} />
     </div>
   );
 };
